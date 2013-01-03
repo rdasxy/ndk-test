a808ff292730f9b5b9b722af81c43565b201c666
Riddhiman Das <rdasxy@gmail.com>
2013-01-02 19:43:14 -0600

    Converted to CPP to test

--- com.galleonlabs.nativetest/jni/Android.mk
+++ com.galleonlabs.nativetest/jni/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := com_example_com_galleonlabs_nativetest_NativeLib.c
+LOCAL_SRC_FILES := com_example_com_galleonlabs_nativetest_NativeLib.cpp
 LOCAL_MODULE := com_example_com_galleonlabs_nativetest_NativeLib
 include $(BUILD_SHARED_LIBRARY)
